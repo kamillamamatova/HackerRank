f08eb68db333e8a535510ced73d38652b940b00c
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-11-08 00:37:24 -0500

    Implemented the python version of birthdayCakeCandles.java

---
+++ birthdayCakeCandles.py
@@ -0,0 +1,12 @@
+def birthdayCakeCandles(candles):
+  tallest = max(candles)
+
+  tallest_count = candles.count(tallest)
+
+  return tallestCount;
+
+if __name__ = "__main__":
+  n = int(input().srip())
+  candles = list(map(int, input().rstrip().split())
+
+  print(birthdayCakeCandles(candles))
