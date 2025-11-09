eaebf0ea35c3734c2692058317af2c36dedc935e
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-11-08 21:54:27 -0500

    .
    Removed commented-out function documentation from aVeryBigSum.

--- aVeryBigSum.java
+++ aVeryBigSum.java
@@ -11,14 +11,6 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
-
-    /*
-     * Complete the 'aVeryBigSum' function below.
-     *
-     * The function is expected to return a LONG_INTEGER.
-     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
-     */
-
     public static long aVeryBigSum(List<Long> ar) {
         long sum = 0;
         
